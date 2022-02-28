7471e9b739b740aa0a82d28e44092a0ec38a6c33
marcodeiulio <mdeiulio@icloud.com>
2022-02-28 17:04:13 +0100

    - Added base array structure

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+$faqs = [
+	[
+		'title' => 'How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?',
+		'paragraphs' => [
+			'The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person\'s name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.',
+			'The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person\'s name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.',
+			'The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person\'s name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.',
+		],
+	],
+	[
+		'title' => 'How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?',
+		'paragraphs' => [
+			'The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person\'s name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.',
+			'The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person\'s name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.',
+			'The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person\'s name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.',
+		],
+	]
+]
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +30,15 @@
 </head>
 
 <body>
+	<?php
+	foreach ($faqs as $faq) : ?>
+		<h2><?= $faq['title'] ?></h2>
 
+		<?php foreach ($faq['paragraphs'] as $p) : ?>
+			<p><?= $p ?></p>
+
+		<?php endforeach ?>
+	<?php endforeach ?>
 </body>
 
 </html>
